478643f8983a0167d6edf7fb3d371dd0bd977fb9
wisatong <wisa.pow@gmail.com>
2019-12-01 23:44:56 +0700

    axios thing

--- src/v1/routes/auth.js
+++ src/v1/routes/auth.js
@@ -13,9 +13,7 @@ async function auth(req, res) {
    * https://developers.facebook.com/docs/instagram-basic-display-api/getting-started#step-5--exchange-the-code-for-a-token
    */
   // EXAMPLE
-  const response = await getToken(code, "529679997616021", "457cec212324e7f754d92beba50a7122");
-  console.log(response);
-
+  const resToken = await getToken(code, "754866428313827", "13e62e7cdc68d87a03b20936404d4392")
 
   /**
    * GET user node /me
@@ -23,7 +21,8 @@ async function auth(req, res) {
    * 
    * { id username } = res ??
    */
-
+  const resMe = await me('IGQVJWa0w1RjVhcFQ5a2oyeHF1SGhiUHVtX0RvWUNUSUlhcUU2NzJaaUd1aUI1QzBLeWl4ZAGNNLVZABMl9reVd4QXhwRFR5dkdnZAUpQeUtnQ01LTU9xdGQ3cW90UUx0eEdaUDFLd0ljenFsWk41SDVKNUZAVRGZAQNHBoV1BB')
+  console.log(resMe.data)
 
   /**
    * GET user medias
@@ -41,24 +40,28 @@ async function auth(req, res) {
 }
 
 async function getToken(code, appId, appSecret) {
-  // return axios.post('https://api.instagram.com/oauth/access_token',null,{
-  //   params: {
-  //     app_id: appId,
-  //     app_secret: appSecret,
-  //     code: code
-  //   }});
-  console.log("code: ",code);
-  console.log("addid: ", appID);
   return axios({
     method: 'post',
     url: 'https://api.instagram.com/oauth/access_token',
-    data: {
+    params: {
       app_id: appId,
       app_secret: appSecret,
-      code: code
+      code: code,
+      grant_type: 'authorization_code',
+      redirect_uri: 'https://127.0.0.1/v1/auth'
     }
   });
-    
+}
+
+async function me(token) {
+  return axios({
+    method: 'get',
+    url: 'https://graph.instagram.com/me',
+    params: {
+      fields: 'id,username',
+      access_token: token
+    }
+  });
 }
 
 export default router;
