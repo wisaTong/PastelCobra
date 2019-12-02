cffcf7af7a63d86f0ade0813895fa2701a8a3316
wisatong <wisa.pow@gmail.com>
2019-12-02 08:52:31 +0700

    BIgimage use all pixel

--- src/front/App.js
+++ src/front/App.js
@@ -21,18 +21,26 @@ function App() {
       .then(res => setBuckets(res.data))
   }, [])
 
+  const allPxls = _.chain(buckets)
+    .map((i) => i.buckets)
+    .flatten()
+    .value();
+
+  const sumBuckets = binPixels(allPxls, 3);
+  console.log(sumBuckets);
+
   return (
     <div className='flex-container'>
-      { buckets.length > 0
+      {buckets.length > 0
         ? <>
           <Navbar img={pic} name={name} />
           <div className='main-container'>
-            <BigImage colors={_.rest(buckets[0].colors.reverse(), -5)} imgUrl={buckets[0].url} />
+            <BigImage colors={_.rest(sumBuckets.colors.reverse(), -5)} imgUrl={buckets[0].url} />
             <SmallImage images={buckets.map(b => b.url)}></SmallImage>
             <Graph imgUrl={pic} ></Graph>
           </div>
         </>
-        : <LoadingOverlay message="loading..."/>}
+        : <LoadingOverlay message="loading..." />}
     </div>
   );
 }
@@ -46,5 +54,54 @@ const LoadingOverlay = ({ message }) => {
   );
 }
 
+const DIMENSION_MAX = 256;
+
+function getKeyForPixel(pixel, bucketsPerDimension) {
+  let bucketSize = DIMENSION_MAX / bucketsPerDimension;
+  let redBucket = Math.floor(pixel.r / bucketSize);
+  let greenBucket = Math.floor(pixel.g / bucketSize);
+  let blueBucket = Math.floor(pixel.b / bucketSize);
+  let key = redBucket + ":" + greenBucket + ":" + blueBucket;
+  return key;
+}
+
+function binPixels(pixels, bucketsPerDimension) {
+  let bucketMap = {};
+  _.each(pixels, (pixel) => {
+    let key = getKeyForPixel(pixel, bucketsPerDimension);
+    if (key in bucketMap) {
+      bucketMap[key].push(pixel);
+    } else {
+      bucketMap[key] = [pixel];
+    }
+  });
+
+  // sort buckets
+  let buckets = _.values(bucketMap);
+  let sortedBuckets = _.sortBy(buckets, (bucket) => { return bucket.length; }).reverse();
+
+  const keys = Object.keys(bucketMap);
+  let bucketColors = _.map(keys, (key, index) => {
+    let pixelsInBucket = bucketMap[key];
+    let averageColor = computeAverageColor(pixelsInBucket);
+    return { percent: (pixelsInBucket.length / pixels.length) * 100, ...averageColor };
+  });
+
+  return {
+    buckets: sortedBuckets,
+    colors: bucketColors
+  };
+}
+
+function computeAverageColor(pixels) {
+  var totalRed = _.reduce(pixels, (m, p) => m + p.r, 0);
+  var totalGreen = _.reduce(pixels, (m, p) => m + p.g, 0);
+  var totalBlue = _.reduce(pixels, (m, p) => m + p.b, 0);
+  return {
+    r: (totalRed / pixels.length),
+    g: (totalGreen / pixels.length),
+    b: (totalBlue / pixels.length)
+  };
+}
 
 export default App;
